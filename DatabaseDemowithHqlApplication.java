a4e5823fe6daf6e495c9a5d819c70d02c83ebc42
sakshi98681 <sakshi98kadam@gmail.com>
2025-04-01 13:29:32 +0530

    Add files via upload

---
+++ DatabaseDemowithHqlApplication.java
@@ -0,0 +1,14 @@
+package com.tka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseDemowithHqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseDemowithHqlApplication.class, args);
+		System.out.println("Application is Started....");
+	}
+
+}
